f11799380dac0f6a162fe85be60bf228acca0c84
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-07-18 23:49:29 +0300

    Initial commit

---
+++ src/test/java/ru/zubov/spring_authorization_server/SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zubov.spring_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
