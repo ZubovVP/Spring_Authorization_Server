ea22d36e8888649b685084ae16cca031629da9c0
Vitaly Zubov <Zubov.VP@yandex.ru>
2023-07-21 23:39:33 +0300

    Solved mistake security

--- src/main/java/ru/zubov/spring_authorization_server/security/SecurityConfig.java
+++ src/main/java/ru/zubov/spring_authorization_server/security/SecurityConfig.java
@@ -1,18 +1,23 @@
 package ru.zubov.spring_authorization_server.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.zubov.spring_authorization_server.entity.User;
 import ru.zubov.spring_authorization_server.repository.UserRepository;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests.anyRequest().authenticated()
@@ -21,10 +26,16 @@ public class SecurityConfig {
                 .and()
                 .build();
     }
+
     @Bean
-    UserDetailsService userDetailsService(UserRepository userRepo) {
-        return userRepo::findByUsername;
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
+        };
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -7,7 +7,3 @@ insert into Role_User (id, rolename) values (0, 'ROLE_ADMIN');
 -- Create admin account (password - admin)
 insert into Users (id, username, password, fullname, phone_number) values (2, 'admin', '$2a$10$v.s1TtjRt9rbl1.NduoOmeOgMBtyTLFd4CqhRj9HCKDpMdToyDKjO', 'admin', '123');
 insert into Users_Authorities (user_id, role_id) values (2, 0);
-
--- Create user account (password - user)
-insert into Users (id, username, password, fullname, phone_number) values (-1, 'user', '$2a$10$rUvsQ4IVlZekge9fBCwXHOfD3mKPwrfARgb7NfIR7pRMLHfGRkvHm', 'user', '123');
-insert into Users_Authorities (user_id, role_id) values (-1, 1);
